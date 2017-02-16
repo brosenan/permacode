fb5dc171e2764bc80a518a82c7ef0079c7b79416
Boaz Rosenan <brosenan@gmail.com>
2017-02-16 22:29:08 +0200

    Deploy to GitHub Pages: f90a0155fc52b603170f1937e8139af62f331a93

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject permacode "0.1.0-SNAPSHOT"
+  :description "A pure dialect of Clojure with content-addressed modules"
+  :url "http://github.com/brosenan/permacode"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.logic "0.8.11"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
+                                  [im.chit/lucid.publish "1.2.8"]
+                                  [im.chit/hara.string.prose "2.4.8"]]
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :publish {:theme  "bolton"
+            :template {:site   "permacode"
+                       :author "Boaz Rosenan"
+                       :email  "brosenan@gmail.com"
+                       :url "https://github.com/brosenan/permacode"}
+            :output "docs"
+            :files {"core"
+                    {:input "test/permacode/core_test.clj"
+                     :title "core"
+                     :subtitle "Subsetting Clojure"}}})
