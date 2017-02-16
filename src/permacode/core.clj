f90a0155fc52b603170f1937e8139af62f331a93
Boaz Rosenan <brosenan@gmail.com>
2017-02-16 22:28:32 +0200

    migrated from pureclj

---
+++ src/permacode/core.clj
@@ -0,0 +1,146 @@
+(ns permacode.core
+  (:use [clojure.set :exclude [project]]))
+
+(declare list-symbols)
+
+(defmulti symbols class)
+(derive Number ::primitive)
+(derive String ::primitive)
+(derive Boolean ::primitive)
+(derive clojure.lang.Keyword ::primitive)
+
+(defmethod symbols ::primitive [expr] #{})
+(defmethod symbols nil [expr] #{})
+
+(defmethod symbols clojure.lang.Symbol [symbol] #{symbol})
+
+(defmethod symbols clojure.lang.ISeq [seq]
+  (apply list-symbols (macroexpand seq)))
+
+(defmethod symbols clojure.lang.PersistentVector [vec]
+  (if (empty? vec)
+    #{}
+    (union (symbols (first vec)) (symbols (rest vec)))))
+
+(defmethod symbols clojure.lang.IPersistentMap [map]
+  (union (symbols (keys map)) (symbols (vals map))))
+
+(defmethod symbols clojure.lang.IPersistentSet [set]
+  (symbols (seq set)))
+
+(defmulti ^:private list-symbols (fn ([first & args] first)
+                           ([] :function-call)) :default :function-call)
+
+(defmethod list-symbols :function-call [& seq]
+  (if (empty? seq)
+    #{}
+    (union (symbols (first seq)) (symbols (rest seq)))))
+
+(defn ^:private bindings-symbols [bindings syms]
+  (if (empty? bindings)
+    syms
+    (let [[pattern val] (take 2 bindings)]
+      (union (symbols val) (difference
+                             (bindings-symbols (drop 2 bindings) syms)
+                             (symbols pattern))))))
+
+(defmethod list-symbols 'let* [_ bindings & expr]
+  (bindings-symbols bindings (symbols expr)))
+
+(defn ^:private fn-bindings [bindings]
+  (if (empty? bindings)
+    #{}
+    (let [[args & body] (first bindings)]
+      (union (difference (symbols body) (symbols args))
+             (fn-bindings (rest bindings))))))
+
+(defmethod list-symbols 'fn*
+  ([_ & bindings]
+   (if (symbol? (first bindings))
+     (difference (fn-bindings (rest bindings)) #{(first bindings)})
+     (fn-bindings bindings))))
+
+(defmethod list-symbols 'def
+  ([_ & args] (throw (Exception. "def is not allowed"))))
+
+(defmethod list-symbols 'quote
+  ([_ quoted] #{}))
+
+(defmethod list-symbols 'if
+  ([_ & args] (symbols args)))
+
+(defmethod list-symbols 'do
+  ([_ & args] (symbols args)))
+
+(defmethod list-symbols 'var
+  ([_ sym] (throw (Exception. "vars are not allowed"))))
+
+(defmethod list-symbols 'loop*
+  ([_ bindings & body] (bindings-symbols bindings (symbols body))))
+
+(defmethod list-symbols 'recur
+  ([_ & exprs] (symbols exprs)))
+
+(defmethod list-symbols 'throw
+  ([_ exception] (throw (Exception. "throw is not allowed. Use error instead"))))
+
+(defmethod list-symbols 'try
+  ([_ & body] (throw (Exception. "try/catch is not allowed"))))
+
+
+(defn ^:private create-bindings [syms env]
+  (apply concat (for [sym syms
+                      :when (empty? (namespace sym))] [sym `(~env '~sym)])))
+
+(defn box [expr env]
+  (let [syms (symbols expr)
+        missing (difference syms (set (keys env)))]
+    (if (empty? missing)
+      ((eval `(fn [~'$env] (let [~@(create-bindings syms '$env)] ~expr))) env)
+      (throw (Exception. (str "symbols " missing " are not defined in the environment"))))))
+
+(declare update-env)
+
+(defmulti ^:private apply-defs (fn [env form & args] form))
+(defmethod apply-defs 'def
+  ([env _ sym value] (assoc env sym (box value env))))
+(defmethod apply-defs 'do [env _ & exprs]
+  (if (empty? exprs)
+    env
+    (let [env (update-env (first exprs) env)]
+      (apply apply-defs env 'do (rest exprs)))))
+
+(defn update-env [expr env]
+  (let [expr (macroexpand expr)]
+    (apply apply-defs env expr)))
+
+(defn ^:private conjunction [& funcs]
+  (fn [x]
+    (if (empty? funcs)
+      true
+      (and ((first funcs) x) ((apply conjunction (rest funcs)) x)))))
+
+(defn add-ns
+  ([ns env] (add-ns ns [] env))
+  ([ns filters env]
+   (let [filters (map (fn [filt] (comp filt first)) filters)
+         filtered (filter (apply conjunction filters) (ns-publics ns))]
+     (-> env
+         (merge filtered)
+         (merge (apply merge (map (fn [[key val]] {(symbol (name ns) (name key)) val}) filtered)))))))
+
+(defn name-filter [re]
+  (comp (partial re-matches re) name))
+
+(defn name-filter-out [re]
+  (complement (name-filter re)))
+
+(def black-list [#"print.*" #".*!" #"ns-.*" #".*agent.*" #".*-ns"])
+
+(def safe-filters
+  (map (fn [re] (name-filter-out re)) black-list))
+
+(def safe-env
+  (->> {}
+       (add-ns 'clojure.core safe-filters)
+       (add-ns 'clojure.set)))
---
+++ test/permacode/core_test.clj
@@ -0,0 +1,163 @@
+(ns permacode.core-test
+  (:require [permacode.core :refer :all]
+            [midje.sweet :refer :all]))
+
+[[:chapter {:title "Introduction"}]]
+"Like other LISPs, Clojure is an imperative functional programming language.
+In many ways, Clojure has made strides towards 'purity', such as intensive use
+of purely-functional data structures, but in other areas, such as Java interop,
+Clojure is as imperative as Java.
+While imperative programming is commonplace, with only few esoteric programming languages
+taking the purely-declarative path, it has some disadvantages."
+
+"permacode is an attempt to create a purely-functional dialect of Clojure.
+This is a stripped-down Clojure, with only the purely-functional parts, including:
+- Functions (well dah...)
+- `let`, `loop`, `if` etc.
+- maps, sets, vectors and lists
+- A subset of the `clojure.core` library functions and macros, as well as
+- `clojure.string`, `clojure.set` and potentially other pure libraries.
+
+It does not include:
+- Variables
+- Atoms, refs, agents, etc.
+- Java interop
+- Imperative functions from `clojure.core`"
+
+[[:chapter {:title "symbols: Extract symbols from expressions"}]]
+"The `symbols` function is the key in our static analysis.  It takes a s-expression,
+and returns a set of symbols defined by this s-expression."
+
+"For Constants, it returns an empty set."
+(fact 
+ (symbols 2) => #{}
+ (symbols true) => #{}
+ (symbols "foo") => #{}
+ (symbols :foo) => #{}
+ (symbols nil) => #{})
+
+"For a symbol, `symbols` returns a set with that symbol."
+(fact
+ (symbols 'x) => #{'x})
+"`symbols` goes into Clojure structures and aggergates symbols from there."
+(fact
+ (symbols '(+ a b)) => #{'+ 'a 'b}
+ (symbols '[1 a 2 b]) => #{'a 'b}
+ (symbols {'x 'y}) => #{'x 'y}
+ (symbols #{'a 'b 3 4 5}) => #{'a 'b})
+
+"In a `let*` special form, the bindings are removed from the returned set."
+(fact
+ (symbols '(let* [x 1 y 2] (+ x y))) => #{'+}
+ (symbols '(let* [x a] x)) => #{'a})
+
+"`symbols` expands macros, so it also handles the more familiar `let` form"
+(fact
+ (symbols '(let [x a] x a)) => #{'a} )
+
+"`loop` is supported similarly."
+(fact
+ (symbols '(loop [x a y b] a x b y c)) => #{'a 'b 'c} )
+
+"In the `fn*` special form (used by the `fn` macro) `symbols` removes the argument names from the set."
+(fact
+ (symbols '(fn [x y] (+ x y a))) => #{'a '+}
+ (symbols '(fn foo [x y] (+ x y a))) => #{'a '+})
+
+"The function name (in named functions) is also removed."
+(fact
+ (symbols '(fn foo [x] (foo x))) => #{} )
+
+"Multi-arity functions are supported as well."
+(fact
+ (symbols '(fn ([x] (+ a x))
+             ([x y] (+ b x y)))) => #{'a 'b '+} )
+"`def` is not allowed inside an expression (it is allowed on the top-level, as described below)."
+(fact
+ (symbols '(def x 2)) => (throws "def is not allowed") )
+
+"Similarly, reference to variables is not allowed."
+(fact
+ (symbols '#'var) => (throws "vars are not allowed") )
+
+"Quoted symbols are ignored."
+(fact
+ (symbols ''(a b c)) => #{} )
+
+"In special forms such as `if` and `do`, the form's name is ignored."
+(fact
+ (symbols '(if a b c)) => #{'a 'b 'c}
+ (symbols '(do a b c)) => #{'a 'b 'c}
+ (symbols '(recur a b)) => #{'a 'b})
+
+"Exceptions are not supported because they use Java interop."
+(fact
+ (symbols '(throw foo)) => (throws Exception "throw is not allowed. Use error instead")
+ (symbols '(try foo bar baz)) => (throws "try/catch is not allowed"))
+
+
+[[:chapter {:title "box: Evaluate expressions through an environment" :tag "box"}]]
+"should return a constant for a constant"
+(fact
+ (box 3.14 {}) => 3.14)
+"should throw an exception if a symbol that exists in expr is not a key in env"
+(fact
+ (box 'x {}) => (throws "symbols #{x} are not defined in the environment") )
+"should assign symbols their value in the environment"
+(fact
+ (box '(+ x 2) {'x 3 '+ *}) => 6 )
+"should work with functions already defined in the environment"
+(fact
+ (let [env (update-env '(defn foo [x] (inc x)) {'inc inc})]
+   (box '(foo 2) env)) => 3 )
+"should work with qualified symbols as long as they are in env"
+(fact
+ (box '(clojure.core/inc 2) (add-ns 'clojure.core {})) => 3 )
+
+
+[[:chapter "update-env: Update an environment based on defs" :tag "update-env"]]
+"should add a key to env of a def form"
+(fact
+               (update-env '(def y (inc x)) {'x 2 'inc inc}) => {'x 2 'y 3 'inc inc} )
+"should apply macros"
+(fact
+      (let [env (update-env '(defn foo [x] (inc x)) {'inc inc})
+            f (env 'foo)]
+        (f 1)) => 2 )
+"should apply all defs in a do block"
+(fact
+ (update-env '(do (def x 1) (def y 2)) {}) => {'x 1 'y 2} )
+
+[[:chapter {:title "add-ns: Import a namespace to an environment" :tag "add-ns"}]]
+
+"should extend env"
+(fact
+ (add-ns 'clojure.core {'x 3}) => #(contains? % 'x))
+"should add all publics from ns"
+(fact
+ (add-ns 'clojure.core {'x 3}) => #(contains? % '+))
+"should apply filters if supplied"
+(fact
+ (add-ns 'clojure.core [(name-filter-out #".*!")] {}) => #(not (contains? % 'swap!)))
+"should apply multiple filters if supplied"
+(fact
+ (add-ns 'clojure.core [(name-filter-out #".*!") (name-filter-out #"print.*")] {}) => #(not (contains? % 'println)))
+"should return an env containing functions"
+(fact
+ (let [env (add-ns 'clojure.core {})
+       f (env 'inc)]
+   (f 2)) => 3 )
+"should also add the fully-qualified names to env"
+(fact
+ (add-ns 'clojure.core {}) => #(contains? % 'clojure.core/+))
+
+[[:chapter {:title "name-filter"}]]
+"should return true for env entries that  match the patter"
+(fact
+ (filter (name-filter #"x+") ['x 'xx 'xy 'yy]) => '(x xx) )
+
+[[:chapter {:title "name-filter-out"}]]
+"should return true for env entries that  match the patter"
+(fact
+ (filter (name-filter-out #"x+") ['x 'xx 'xy 'yy]) => '(xy yy) )
+
